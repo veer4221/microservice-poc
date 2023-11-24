fa7480f66041a8dddec5c4779dfce8de62fd383d
vinay-satva-4221 <vinayp08042@gmail.com>
2023-11-24 22:21:10 +0530

    feat ( resilliunce4j ) : integrate with Cricut Brucker

--- UserService/UserService/src/main/java/com/lcwd/user/service/controller/UserController.java
+++ UserService/UserService/src/main/java/com/lcwd/user/service/controller/UserController.java
@@ -2,6 +2,10 @@ package com.lcwd.user.service.controller;
 
 import com.lcwd.user.service.entities.User;
 import com.lcwd.user.service.services.UserService;
+import com.lcwd.user.service.services.impl.UserServiceImpl;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +20,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody  User user){
         User user1 = userService.saveUser(user);
@@ -23,9 +29,22 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
         User user = userService.getUser(userId);
         return  ResponseEntity.ok(user);
+    }
+
+    //ratingHotelFallback method
+    public ResponseEntity<User> ratingHotelFallback(String userId ,Exception ex){
+        logger.info("FALLBACK IS EXECUTED BCZ SERVICE IS DOWN: ", ex.getMessage());
+        User user = User.builder()
+                .email("demo@gmail.com")
+                .name("Demo")
+                .about("this is demo user ")
+                .userId("12345")
+                .build();
+        return new ResponseEntity<>(user , HttpStatus.OK);
     }
 
     @GetMapping
